f94082064c6697f13f3f32317ab22b0318f62ef3
Saman Beheshtian (YiiMan) <amintado@gmail.com>
2025-07-11 23:37:35 +0000

    Bacause original php docker images not support        linux/ppc64le           - linux/riscv64           - linux/s390x           we removed this platforms

--- deploy/cpu-arch.sh
+++ deploy/cpu-arch.sh
@@ -10,9 +10,6 @@ declare -a archs_to_add=(
   "amd64"      # linux/amd64
   "arm64"      # linux/arm64, linux/arm64/v8
   "armhf"      # linux/arm/v7 (armhf)
-  "ppc64el"    # linux/ppc64le (Debian uses ppc64el)
-  "riscv64"    # linux/riscv64
-  "s390x"      # linux/s390x
   # Windows platforms usually don't require adding architectures here
 )
 
